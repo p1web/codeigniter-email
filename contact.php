f736769106acca984f53c9b21f53d993c68f7c81
Pawan Dubey <40168397+p1web@users.noreply.github.com>
2020-05-15 13:17:47 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -19,75 +19,57 @@ var base_url = '<?= base_url() ?>';
 </head>
 
 <body>
-
 	<div id="colorlib-blog">
 		<div class="container">
-			<div class="row">
-					
-				<h3>Contact Information</h3>
-				<div class="row contact-info-wrap">
+			<div class="row">			
+			<div class="card mb-3">
+			<div class="card-header"><h3>Get In Touch</h3></div>
+			<div class="card-body">
+			<form action="#" id="contactForm">
+				<div class="row form-group">
 					<div class="col-md-6">
-						
-						<p><span><i class="icon-location-2"></i></span> Flat No. T-6 Asma Complex, Berasia Road Bhopal</p>
+						<label>First Name</label>
+						<input type="text" name="fname" id="fname" class="form-control" placeholder="Your firstname">
 					</div>
-					<div class="col-md-3">
-						<p><span><i class="icon-phone3"></i></span> <a href="tel://9300366312">+91 9300366312</a></p>
+					<div class="col-md-6">
+						<label>Last Name</label>
+						<input type="text" name="lname" id="lname" class="form-control" placeholder="Your lastname">
 					</div>
-					<div class="col-md-3">
-						<p><span><i class="icon-paperplane"></i></span> <a href="mailto:info@hamdard.org">info@hamdard.org</a></p>
-					</div>
-					<!-- <div class="col-md-4">
-						<p><span><i class="icon-globe"></i></span> <a href="#">hamdard.org</a></p>
-					</div> -->
 				</div>
-				<div class="card mb-3">
-				<div class="card-header"><h3>Get In Touch</h3></div>
-				<div class="card-body">
-				<form action="#" id="contactForm">
-					<div class="row form-group">
-						<div class="col-md-6">
-							<label>First Name</label>
-							<input type="text" name="fname" id="fname" class="form-control" placeholder="Your firstname">
-						</div>
-						<div class="col-md-6">
-							<label>Last Name</label>
-							<input type="text" name="lname" id="lname" class="form-control" placeholder="Your lastname">
-						</div>
-					</div>
 
-					<div class="row form-group">
-						<div class="col-md-12">
-							<label>Email</label>
-							<input type="text" name="email" id="email" class="form-control" placeholder="Your email address">
-						</div>
+				<div class="row form-group">
+					<div class="col-md-12">
+						<label>Email</label>
+						<input type="text" name="email" id="email" class="form-control" placeholder="Your email address">
 					</div>
-
-					<div class="row form-group">
-						<div class="col-md-12">
-							<label>Subject</label>
-							<input type="text" id="subject" name="subject" class="form-control" placeholder="Your subject of this message">
-						</div>
-					</div>
-
-					<div class="row form-group">
-						<div class="col-md-12">
-							<label for="message">Message</label>
-							<textarea name="message" id="message" cols="30" rows="10" class="form-control" placeholder="Say something about us"></textarea>
-						</div>
-					</div>
-					<div class="form-group text-center">
-						<input type="submit" value="Send Message" class="btn btn-primary">
-					</div>
-
-				</form>
-				<div id="error_message"></div>
-			
 				</div>
-				</div>			
-				
+
+				<div class="row form-group">
+					<div class="col-md-12">
+						<label>Subject</label>
+						<input type="text" id="subject" name="subject" class="form-control" placeholder="Your subject of this message">
+					</div>
+				</div>
+
+				<div class="row form-group">
+					<div class="col-md-12">
+						<label for="message">Message</label>
+						<textarea name="message" id="message" cols="30" rows="10" class="form-control" placeholder="Say something about us"></textarea>
+					</div>
+				</div>
+				<div class="form-group text-center">
+					<input type="submit" value="Send Message" class="btn btn-primary">
+				</div>
+
+			</form>
+			<div id="error_message"></div>
+
 			</div>
-		</div>
+		</div>			
+
 	</div>
+</div>
+</div>
 
 </body>
 <script src="<?= base_url('asset/web/js/jquery.min.js') ?>"></script> 
